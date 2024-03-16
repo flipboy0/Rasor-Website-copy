11eefe2fdaccd754b13cc8313192895862cd6dbf
Skand Kumar Mishra <skandkm0001@gmail.com>
2024-03-16 20:09:58 +0530

    fixed corosol and other conflicts

--- src/components/Home_page/Collaborate/collaborate.jsx
+++ src/components/Home_page/Collaborate/collaborate.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import Typical from "react-typical";
+import React from 'react';
+import Typewriter from 'typewriter-effect';
 
 function Collaborate() {
     return (
@@ -8,10 +8,23 @@ function Collaborate() {
                 <div className=" bg-white w-full roboto-slab-500 sm:text-2xl text-2xl lg:text-3xl flex flex-col justify-center items-center space-y-2 sm:space-y-3 lg:space-y-4">
                     <h1 className="text-[#040404] font-medium">Want to</h1>
                     <h1 className="font-semibold bg-gradient-to-r from-[#DFA23A] via-[#FFC660] to-[#E3AF52] bg-clip-text text-3xl md:text-4xl lg:text-5xl text-transparent">
-                        <Typical loop={Infinity} wrapper="p" steps={["COLLABORATE?",3000,"ASSOCIATE?",3000,"GROW?",3000,]}></Typical></h1>
+                        <Typewriter
+                            onInit={(typewriter) => {
+                                typewriter.typeString('COLLABORATE?')
+                                    .pauseFor(2000)
+                                    .deleteAll()
+                                    .typeString('ASSOCIATE?')
+                                    .pauseFor(2000)
+                                    .deleteAll()
+                                    .typeString('GROW?')
+                                    .pauseFor(2000)
+                                    .start();
+                            }}
+                        />
+                    </h1>
                 </div>
 
-                <button className="relative border-2 border-red-800 bg-transparent py-3 px-6 font-medium uppercase text-3xl lg:w-70 md:w-3/4 sm:w-3/4 w-3/4 text-red-800 transition-colors before:absolute before:left-0 before:top-0 before:-z-10 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-red-800 before:transition-transform before:duration-300 before:content-[''] hover:text-white before:hover:scale-x-100 inline-block">
+                <button className=" relative z-[1] -top-1 -left-1 bg-indigo-800 py-2.5 px-5 font-medium  text-white transition-all before:absolute before:top-1 before:left-1 before:h-full before:w-full before:border-2 before:border-indigo-700 before:transition-all before:content-[''] hover:top-0 hover:left-0 before:hover:top-0 before:hover:left-0">
                     Contact Us
                 </button>
             </div>
