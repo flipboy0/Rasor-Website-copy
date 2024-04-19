c52840bf39bd0d0ca7f6f7d5c1daa5d428b4be77
Chhavikant Mahobia <ckcodes907@gmail.com>
2024-04-19 20:04:04 +0530

    announcement text animation added

--- src/components/Announcement/Announcement.jsx
+++ src/components/Announcement/Announcement.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
-
+import "./Announcement.css"
 export const Announcement = () => {
   return (
-    <div className='flex w-full h-[120px] mt-5 text-xl'>
-        <div className='flex justify-center text-white w-[15%] h-full items-center bg-[#DFA43A] text-center'>Announcement</div>
-        <div className='flex w-[85%] justify-around items-center border-y-2'>
-            <h1>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus culpa alias distinctio quos, quidem, quas,
-            </h1>
-            <button className='border-[3px] border-[#861D1D] h-[60px] w-[120px] text-[#861D1D]'>
+    <div className='border-y-2 pr-10 flex items-center justify-between w-full h-[70px] mt-5 text-xl'>
+      
+        <div className='flex w-[20%] justify-center text-white h-full items-center bg-[#DFA43A] text-center'>Announcement</div>
+        <div className='flex w-[72%] h-full justify-around items-center pr-5'>
+          <p class="marquee">
+            <span className='text-[#C41E3A]'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta odio dolorem optio, quia ullam in? Minima aliquid nemo perferendis facere corporis porro maxime. -&nbsp;</span>
+          </p>  
+        </div>
+        <div className='w-[8%] h-full flex items-center justify-center pr-5'>
+        <button className='w-full text-center border-[3px] border-[#861D1D] h-[45px] text-[#861D1D]'>
                 Expand
-            </button>
+        </button>
         </div>
     </div>
   )
