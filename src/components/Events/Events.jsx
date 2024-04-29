6c7e1d1ac5f87e8d8a834ae6998a63996c457ecc
Chhavikant Mahobia <ckcodes907@gmail.com>
2024-04-29 22:47:51 +0530

    card animation

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -40,4 +40,32 @@ const Events = () => {
   )
 }
 
+export const Card = ({ item }) => {
+    return (
+      <div>
+          <div class="card rounded-lg w-[340px] h-[427px] sm:w-[390px] sm:h-[490px] shadow-md overflow-hidden relative">
+              <div className="w-[340px] h-[427px] sm:w-[390px] sm:h-[490px] rounded-lg shadow-md overflow-hidden relative">
+                  <img src={item.image} className="poster duration-[400ms] w-full h-full object-scale-down " />
+              </div>
+              
+            
+              <div class="faustina-300 card-body rounded-lg w-full h-full absolute top-0 bg-[#000000b6] backdrop-blur-[5px]  text-[#fff] p-[30px] flex flex-col justify-center right-[-100%] duration-[450ms]">
+                  <h1 class="text-4xl">{item.title}</h1>
+                  <p class="text-xl roboto-condensed-300 leading-[25px] mt-[40px] pb-10">{item.description}</p>
+  
+                  <a href={item.link} className="text-[#bf3838] roboto-condensed-300 text-xl flex items-center justify-center bg-[#fff] rounded-[5px] py-2">
+                    <button className="flex items-center">Watch on 
+                      <img className='w-[32px] h-[20px] pl-2 pr-1' src={YouTube} />
+                    YouTube</button>
+                  </a>
+  
+              </div>
+              
+          </div>
+        
+         
+      </div>
+    );
+  };
+
 export default Events
